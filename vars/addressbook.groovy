e57ca6a80e95f4c24de6fc13fb1d84a811499db1
praneethss <38975622+praneethss@users.noreply.github.com>
2022-04-08 13:10:45 +0530

    Update addressbook.groovy

--- vars/addressbook.groovy
+++ vars/addressbook.groovy
@@ -3,6 +3,6 @@ def call() {
     checkout([
         $class: 'GitSCM',
         branches: [[name: '*/master']],
-        userRemoteConfigs: [[ url: https://github.com/praneethss/addressbook.git ]]
+        userRemoteConfigs: [[ url: "https://github.com/praneethss/addressbook.git" ]]
     ])
 }
